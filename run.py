3e7d5e66fe370ec046d850a25aa8a1e8f97673cb
Matej <forgac.matej@gmail.com>
2021-09-02 11:12:11 +0200

    added path as line argument, done

--- run.py
+++ run.py
@@ -1,27 +1,25 @@
 from crontab import CronTab
 import getpass
 import os
-import sys
+import click
 
 
-def check_switcher_to_delete() -> bool:  # return True, if line argument was -d
-
-    if len(sys.argv) >= 2 and sys.argv[1] == "-d":
-        return True
-    return False
-
-
-def run_crontab():
+@click.command()
+@click.option("--path", "path_to_dir", type=str, required=True,
+              help="Path to directory where .log files will be compressed")
+@click.option("--d", "delete_file", is_flag=True,
+              help="Delete all .log files after they were gzipped.")
+def run_crontab(path_to_dir: str, delete_file: bool = False):
     path: str = os.path.abspath(os.getcwd())
     path_to_file: str = path + "/compress.py"
 
     cron = CronTab(user=getpass.getuser())  # get current user
-    if check_switcher_to_delete():
-        job = cron.new(command=f'/usr/bin/python3 {path_to_file} -d')
+    if delete_file:
+        job = cron.new(command=f"/usr/bin/python3 {path_to_file} --path {path_to_dir} --d")
     else:
-        job = cron.new(command=f'/usr/bin/python3 {path_to_file}')
+        job = cron.new(command=f"/usr/bin/python3 {path_to_file} --path {path_to_dir}")
 
-    job.setall('0 5 */30 * *')  # set every month
+    job.setall("0 5 */30 * *")  # set every month
 
     cron.write()
 
